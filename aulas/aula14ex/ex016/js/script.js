6e3e16d4a89c0e5056a7f72f94c01ad993d0be87
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-02-02 13:34:04 -0300

    att

--- aulas/aula14ex/ex016/js/script.js
+++ aulas/aula14ex/ex016/js/script.js
@@ -1,15 +1,37 @@
-const contar = document.querySelector('#btn');
-contar.addEventListener('click', clicar);
-const contagem = document.querySelector('#contagem');
-function clicar() {
-    let inicio = Number(document.querySelector('#inicio').value);
-    let fim = Number(document.querySelector('#fim').value);
-    let passo = Number(document.querySelector('#passo').value);
+function contar() {
+    let inicio = document.querySelector('#inicio');
+    let fim = document.querySelector('#fim');
+    let passo = document.querySelector('#passo');
+    let res = document.querySelector('#res');
     let c = '';
-    for (inicio; inicio <= fim; inicio += passo) {
-        if (inicio < fim) {
-            c += `${inicio} &#x1F449; `;
+
+    if (
+        inicio.value.length == 0 ||
+        fim.value.length == 0 ||
+        passo.value.length == 0
+    ) {
+        alert('[ERRO] Preencha os campos!');
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: <br>';
+        let i = Number(inicio.value);
+        let f = Number(fim.value);
+        let p = Math.abs(Number(passo.value));
+        if (p == 0) {
+            alert('PASSO INVÁLIDO! Considerando PASSO = 1')
+            p = 1
         }
-        contagem.innerHTML = `${c} &#x1F3F4;`;
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1f449} `;
+            }
+        } else {
+            // Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1f449} `
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`;
     }
 }
